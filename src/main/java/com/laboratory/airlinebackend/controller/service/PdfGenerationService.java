e451e2e27dc8fedee21ddffc6048b7e64c096ade
brayanandrey <114691839+brayanandrey@users.noreply.github.com>
2023-11-30 23:07:42 -0500

    Se agregó los detalles del vuelo al pdf de confirmación de check-in

--- src/main/java/com/laboratory/airlinebackend/controller/service/PdfGenerationService.java
+++ src/main/java/com/laboratory/airlinebackend/controller/service/PdfGenerationService.java
@@ -1,10 +1,10 @@
 package com.laboratory.airlinebackend.controller.service;
 
 import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.itextpdf.text.Image;
+import com.itextpdf.text.*;
+import com.laboratory.airlinebackend.controller.DTO.ConsultCheckInDTO;
 import org.springframework.stereotype.Service;
 
-import com.itextpdf.text.Document;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
@@ -16,7 +16,7 @@ import java.io.IOException;
 @Service
 public class PdfGenerationService {
 
-    public byte[] generatePdf(String content) {
+    public byte[] generatePdf(ConsultCheckInDTO checkInDTO) {
         Document document = new Document();
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
@@ -25,11 +25,13 @@ public class PdfGenerationService {
             document.open();
 
             // Add content to the PDF
-            //document.add(new Paragraph(content));
+            addContentToPdf(document, checkInDTO);
 
             // Add QR code to the PDF
+            String content = checkInDTO.toString();
             byte[] qrCodeImage = generateQrCode(content);
             Image image = Image.getInstance(qrCodeImage);
+            //image.scaleToFit(100, 100);
             document.add(image);
 
         } catch (Exception e) {
@@ -49,5 +51,28 @@ public class PdfGenerationService {
         MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
 
         return outputStream.toByteArray();
+    }
+
+    private void addContentToPdf(Document document, ConsultCheckInDTO checkInDTO) throws DocumentException {
+        // Add title
+        Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 16);
+        Paragraph title = new Paragraph("Boarding Pass", titleFont);
+        title.setAlignment(Element.ALIGN_CENTER);
+        document.add(title);
+
+        // Add information
+        Font infoFont = FontFactory.getFont(FontFactory.HELVETICA, 12);
+        document.add(new Paragraph("Flight ID: " + checkInDTO.getFlightId(), infoFont));
+        document.add(new Paragraph("Passenger ID: " + checkInDTO.getPassengerId(), infoFont));
+        document.add(new Paragraph("Email: " + checkInDTO.getEmail(), infoFont));
+        document.add(new Paragraph("Name: " + checkInDTO.getFirstName() + " " + checkInDTO.getLastName(), infoFont));
+        document.add(new Paragraph("DNI: " + checkInDTO.getDNI(), infoFont));
+        document.add(new Paragraph("Check-in Status: " + (checkInDTO.isDidCheckIn() ? "Checked In" : "Not Checked In"), infoFont));
+        document.add(new Paragraph("Origin: " + checkInDTO.getOrigin(), infoFont));
+        document.add(new Paragraph("Destination: " + checkInDTO.getDestination(), infoFont));
+        document.add(new Paragraph("Flight Date: " + checkInDTO.getFlightDate(), infoFont));
+        document.add(new Paragraph("State: " + checkInDTO.getState(), infoFont));
+        document.add(new Paragraph("Seat ID: " + checkInDTO.getSeatId(), infoFont));
+        document.add(new Paragraph("Seat Number: " + checkInDTO.getSeatNumber() + " " + checkInDTO.getSeatLetter(), infoFont));
     }
 }
